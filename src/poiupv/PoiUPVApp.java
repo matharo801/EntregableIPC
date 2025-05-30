2d554d464a304c3f32edcc3d94aaa3f5d65b3524
The_T <The_T@10.237.32.73>
2025-05-30 09:25:30 +0200

    Initial commit

--- src/poiupv/PoiUPVApp.java
+++ src/poiupv/PoiUPVApp.java
@@ -12,27 +12,18 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-/**
- *
- * @author jose
- */
 public class PoiUPVApp extends Application {
-    
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Principal.fxml"));
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/logo.png")));
         Scene scene = new Scene(root);
-        stage.setTitle("Puntos de interes UPV");
+        stage.setTitle("Menu Principal");
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
